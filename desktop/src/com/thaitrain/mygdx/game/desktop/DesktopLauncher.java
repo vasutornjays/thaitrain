13319a3dc3073413157f4c02df6767856a427783
vasutornjays <vasutorn.s@ku.th>
2016-10-14 14:09:03 +0700

    init project

---
+++ desktop/src/com/thaitrain/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.thaitrain.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.thaitrain.mygdx.game.ThaitrainGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ThaitrainGame(), config);
+	}
+}
